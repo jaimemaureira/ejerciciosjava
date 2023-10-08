0c81e4353134449278f28fe412e326f100d4a0fa
Jaime Maureira <jaime_maureira_a@hotmail.es>
2023-10-08 19:51:24 -0300

    creando class

--- src/Ateneo/Catalogo.java
+++ src/Ateneo/Catalogo.java
@@ -7,4 +7,56 @@ package Ateneo;
  */
 public class Catalogo {
     
+    protected String idIsbn, titulo, autor;
+    protected int precio;
+
+    public Catalogo() {
+    }
+
+    public Catalogo(String idIsbn, String titulo, String autor, int precio) {
+        this.idIsbn = idIsbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+
+    public String getIdIsbn() {
+        return idIsbn;
+    }
+
+    public void setIdIsbn(String idIsbn) {
+        this.idIsbn = idIsbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalogo{" + "idIsbn=" + idIsbn + ", titulo=" + titulo + ", autor=" + autor + ", precio=" + precio + '}';
+    }
+    
+    
+    
 }
